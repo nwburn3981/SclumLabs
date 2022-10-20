645ff6ff852530752c3dbde1aa148972a7c6dead
Nicholas Burns <42648266+nwburn3981@users.noreply.github.com>
2022-10-20 10:24:42 -0400

    5th exercise done

---
+++ Labs/AngularCh1/cc-template-vars/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cc-template-vars';
+
+  addNewPicture(gallery: GalleryComponent) {
+    gallery.pictures.unshift(gallery.generateImage());
+    console.log('added new picture');
+  }
+
+  removeFirstPicture(gallery: GalleryComponent) {
+    gallery.pictures.shift();
+    console.log('removed first picture');
+  }
+}
