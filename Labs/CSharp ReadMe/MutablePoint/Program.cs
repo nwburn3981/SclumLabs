614f656cd739d789af4961f4301c5d0b9647e627
Nicholas Burns <42648266+nwburn3981@users.noreply.github.com>
2022-10-25 17:16:10 -0400

    Progressing through read me

---
+++ Labs/CSharp ReadMe/MutablePoint/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MutablePoint
+{
+
+    public struct MutablePoint {
+        public int X;
+        public int Y;
+
+        public MutablePoint(int x, int y) => (X, Y) = (x, y);
+
+        public override string ToString() => $"({X}, {Y})";
+      
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new MutablePoint(1, 2);
+            var p2 = p1;
+            p2.Y = 200;
+            Console.WriteLine($"{nameof(p1)} after {nameof(p2)} is modified: {p1}");
+            Console.WriteLine($"{nameof(p2)}: {p2}");
+
+            MutateAndDisplay(p2);
+            Console.WriteLine($"{nameof(p2)} after passing to a method: {p2}");
+        }
+        private static void MutateAndDisplay(MutablePoint p)
+        {
+            p.X = 100;
+            Console.WriteLine($"Point mutated in a method: {p}");
+        }
+    }
+
+    
+}
